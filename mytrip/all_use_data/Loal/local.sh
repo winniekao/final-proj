3a53127f1b9455ee3bf4ce3ce6de4a9c16f1517b
winniekao <winniekao0122@gmail.com>
2015-12-05 17:12:08 +0800

    3 commit 20151205

---
+++ mytrip/all_use_data/Loal/local.sh
@@ -0,0 +1,4 @@
+cut -f 1 local > local_id
+cut -f 2 local > longitude
+cut -f 3 local > latitude
+cut -f 4 local > local_name
---
+++ mytrip/all_use_data/Trip/trip.sh
@@ -0,0 +1,7 @@
+cut -f 1 mytripApp_trip  > uid_id_id
+cut -f 2 mytripApp_trip  > trip_is
+cut -f 3 mytripApp_trip  > trip_name
+cut -f 4 mytripApp_trip  > trip_date
+cut -f 5 mytripApp_trip  >likes
+cut -f 6 mytripApp_trip  > counts
+
